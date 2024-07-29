b2107e095f3bdcbf61eb1958f39631506c4a78ba
uyf <spr47@mail.ru>
2024-07-29 17:53:34 +0700

    added tests 3.2

--- test_todo.py
+++ test_todo.py
@@ -1,4 +1,5 @@
 import re
+import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
@@ -9,5 +10,22 @@ def test_add_todo(page):
     page.get_by_placeholder("What needs to be done?").press("Enter")
     page.get_by_label("Toggle Todo").check()
     # page.locator('').click()
+def test_checkbox(page):
+    page.goto('https://zimaev.github.io/checks-radios/')
+    page.locator("text=Default checkbox").click()
+    page.locator("text=Checked checkbox").click()
+    page.locator("text=Default radio").click()
+    page.locator("text=Default checked radio").click()
+    page.locator("text=Checked switch checkbox input").click()
 
-# asd
+
+def test_select(page):
+    page.goto("https://zimaev.github.io/select/")
+    page.select_option('#floatingSelect', value="3")
+    page.select_option('#floatingSelect', index=1)
+    page.select_option('#floatingSelect', label="Нашел и завел bug")
+
+@pytest.mark.smoke
+def test_multi(page):
+    page.goto("https://zimaev.github.io/select/")
+    page.select_option('#skills', ["playwright", "python"])
