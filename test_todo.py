0c1eb85296b69c9b9c3340bb29e97a3bfab8dbef
uyf <spr47@mail.ru>
2024-08-02 11:43:22 +0700

    added test class

--- test_todo.py
+++ test_todo.py
@@ -10,6 +10,8 @@ def test_add_todo(page):
     page.get_by_placeholder("What needs to be done?").press("Enter")
     page.get_by_label("Toggle Todo").check()
     # page.locator('').click()
+
+
 def test_checkbox(page):
     page.goto('https://zimaev.github.io/checks-radios/')
     page.locator("text=Default checkbox").click()
@@ -25,7 +27,83 @@ def test_select(page):
     page.select_option('#floatingSelect', index=1)
     page.select_option('#floatingSelect', label="Нашел и завел bug")
 
-@pytest.mark.smoke
+
 def test_multi(page):
     page.goto("https://zimaev.github.io/select/")
     page.select_option('#skills', ["playwright", "python"])
+
+
+def test_drag_and_drop(page):
+    page.goto('https://zimaev.github.io/draganddrop/')
+    page.drag_and_drop('#drag', '#drop')
+
+
+def test_dialog(page):
+    page.goto('https://zimaev.github.io/dialog/')
+    page.on('dialog', lambda dialog: dialog.accept())
+    page.get_by_text('Диалог Confirmation').click()
+
+
+def test_select_file(page):
+    page.goto('https://zimaev.github.io/upload/')
+    page.set_input_files('#formFile', 'hello.txt')
+    page.locator("#file-submit").click()
+
+
+def test_table(page):
+    page.goto('https://zimaev.github.io/table/')
+    row = page.locator("tr")
+    print(row.all_text_contents())
+
+
+def test_new_tab(page):
+    page.goto("https://zimaev.github.io/tabs/")
+    with page.context.expect_page() as tab:
+        page.get_by_text("Переход к Dashboard").click()
+
+    new_tab = tab.value
+    assert new_tab.url == "https://zimaev.github.io/tabs/dashboard/index.html?"
+    sign_out = new_tab.locator('.nav-link', has_text='Sign out')
+    assert sign_out.is_visible()
+
+
+# FIELD_LOCATOR = '.new-todo'
+#
+# @pytest.mark.fail
+# def test_two_task(page):
+#     page.goto('https://demo.playwright.dev/todomvc/#/')
+#     page.locator(FIELD_LOCATOR).fill('сварить пельмени')
+#     page.locator(FIELD_LOCATOR).press('Enter')
+#     page.locator(FIELD_LOCATOR).fill('бахнуть пельмени')
+#     page.locator(FIELD_LOCATOR).press('Enter')
+#     page.pause()
+#     expect(page.locator('[data-testid="todo-title"]')).to_have_count(3)
+
+
+@pytest.mark.smoke
+class TestForThree:
+    FIELD_LOCATOR = '.new-todo'
+
+    def test_check_url(self, page):
+        page.goto('https://demo.playwright.dev/todomvc/#/')
+        expect(page, 'Другой урл').to_have_url('https://demo.playwright.dev/todomvc/#/')
+
+    def test_empty(self, page):
+        page.goto('https://demo.playwright.dev/todomvc/#/')
+        expect(page.locator(self.FIELD_LOCATOR)).to_be_empty()
+
+    @pytest.mark.fail
+    def test_two_task(self, page):
+        page.goto('https://demo.playwright.dev/todomvc/#/')
+        page.locator(self.FIELD_LOCATOR).fill('сварить пельмени')
+        page.locator(self.FIELD_LOCATOR).press('Enter')
+        page.locator(self.FIELD_LOCATOR).fill('бахнуть пельмени')
+        page.locator(self.FIELD_LOCATOR).press('Enter')
+        expect(page.locator('[data-testid="todo-title"]')).to_have_count(3)
+
+    def test_one_done_task(self, page):
+        page.goto('https://demo.playwright.dev/todomvc/#/')
+        page.locator(self.FIELD_LOCATOR).fill('сварить пельмени')
+        page.locator(self.FIELD_LOCATOR).press('Enter')
+        page.locator('[type="checkbox"]').nth(0).click()
+        expect(page.get_by_test_id('todo-item').nth(0)).to_have_class('completed')
